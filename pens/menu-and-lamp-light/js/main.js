b4cdca66a89339d00960200db92aeeaf3f962f74
Sasha Soch <sochavaag@gmail.com>
2018-09-28 17:20:37 +0300

    PEN menu-and-lamp-light

--- pens/menu-and-lamp-light/js/main.js
+++ pens/menu-and-lamp-light/js/main.js
@@ -1,59 +1,68 @@
 (function ($) {
   $(function () {
 
-    var agLanguageCurrent = $('.js-language_current'),
+    var agLoginBtn = $('.js-start-login_link'),
+      agLoginPanel = $('.js-start-login_panel'),
+
+      agLanguageCurrent = $('.js-language_current'),
       agLanguagePanel = $('.js-language_panel'),
       agLanguageLocal = $('.js-language_item'),
+      agLanguageBox = $('.js-language_box'),
 
       agSearchBlock = $('.js-search-block'),
       agSearchBtn = $('.js-search_btn'),
       agCloseBtn = $('.js-search_btn-close'),
-      agSubmitBtn = $('#ag-search_btn-submit'),
+      agSubmitBtn = $('#ag-search_btn-submit');
 
-      agLoginPanel = $('.ag-start-login_panel'),
-      agLoginBtn = $('.js-start-login_link');
-
-    function agPanelToggle(btn, panel) {
-      btn.next(panel).fadeToggle();
+    function agPanelShow(btn, panel) {
+      if (panel.css('display') != 'block') {
+        btn.next(panel).fadeIn();
+      }
     }
 
-    function agPanelClose(btn, panel) {
+    function agPanelClose(panel) {
       $(document).bind('keyup', function (e) {
         if (e.keyCode != 27) {
           return true;
         }
-        /* -------------- 'Esc' key (27) -------------- */
+        /* 'Esc' key (27) */
         if (e.keyCode == 27 && panel.is(':visible')) {
           panel.fadeOut();
         }
       });
 
       $(document).mouseup(function (e) {
-        if (!panel.is(e.target) && !btn.is(e.target) && panel.has(e.target).length === 0) {
+        if (!panel.is(e.target) && panel.has(e.target).length === 0) {
           panel.fadeOut();
         }
       });
     }
 
-
     /* login */
     agLoginBtn.on('click', function (e) {
       e.preventDefault();
 
-      agPanelToggle(agLoginBtn, agLoginPanel);
+      agLoginBtnWidth = agLoginBtn.width();
+      agLanguageBtnWidth = agLanguageBox.width();
+      agPanelPosition = (agLanguageBtnWidth + 4),
+      agArrowPosition = ((Math.floor(agLoginBtnWidth / 2) + agLanguageBtnWidth) - 4);
+
+      agLoginPanel.css({right: - agPanelPosition});
+      $('.js-start-login_panel-arrow').css({right: agArrowPosition});
+      agPanelShow(agLoginBtn, agLoginPanel);
     });
 
-    agPanelClose(agLoginBtn, agLoginPanel);
+    agPanelClose(agLoginPanel);
     /* /login */
 
     /* language */
     agLanguageCurrent.on('click', function () {
       agLanguageCurrent.toggleClass('js-ag-language_current-show');
 
-      agPanelToggle(agLanguageCurrent, agLanguagePanel);
+      agPanelShow(agLanguageCurrent, agLanguagePanel);
     });
 
-    agPanelClose(agLanguageCurrent, agLanguagePanel);
+    agPanelClose(agLanguagePanel);
 
     agLanguageLocal.on('click', function () {
       var agLanguageCurrentName = $(this).children('a').html();
@@ -61,10 +70,13 @@
       agLanguageLocal.removeClass('js-ag-language_item-active');
       $(this).addClass('js-ag-language_item-active');
       agLanguageCurrent.html(agLanguageCurrentName);
-      $('.ag-language_panel-arrow').css({right: Math.floor(agLanguageCurrent.width() / 2) - 5}); // calculates the width agLanguageCurrent and aligns the arrow of the panel in the middle agLanguageCurrent
+      $('.js-language_panel-arrow').css({right: Math.floor(agLanguageCurrent.width() / 2) - 5}); // calculates the width agLanguageCurrent and aligns the arrow of the panel in the middle agLanguageCurrent
     });
     /* /language */
 
+
+
+    /* search */
     agSearchBtn.on('click', function () {
       agSearchBtn.next(agSearchBlock).addClass('js-ag-search-block');
       agCloseBtn.show();
@@ -81,14 +93,38 @@
     agSubmitBtn.on('click', function () {
       $('#ag-search_form').submit();
     });
+    /* /search */
 
+    /* mobile menu */
+    $('.js-menu_btn-wrap').on('click', function () {
+      $(this).children().toggleClass('js-ag-menu_btn-show');
+    });
 
-    $('.js-menu-parent_item').on('click', function (e) {
+    $('.js-menu_title-list__subcat').on('click', function (e) {
       e.preventDefault();
 
-      $(this).children('.js-menu-subcat_list').slideToggle();
+      $(this).next('.js-menu-subcat_list').toggleClass('js-ag-menu-subcat_list');
+      $(this).next('.js-menu-subcat_list').slideToggle();
       $(this).toggleClass('js-ag-menu-parent_item');
     });
+
+    var agLanguageSelectbox = $('#ag-mob-language_selectbox'),
+      agGetClass = agLanguageSelectbox.attr('class');
+
+    function agGetFlag() {
+      var agLanguageSelectbox = $('#ag-mob-language_selectbox'),
+        agLanguageCode = $('.js-mob-language_select option:selected').val(),
+        agLanguageFlag = 'ag-language_icon-' + agLanguageCode;
+
+        agLanguageSelectbox.attr('class', agGetClass).addClass(agLanguageFlag);
+    }
+
+    agGetFlag();
+
+    $('.js-mob-language_select').change(function () {
+      agGetFlag();
+    });
+    /* /mobile menu */
 
   });
 })(jQuery);
