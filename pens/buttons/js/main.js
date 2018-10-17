60305b8f57ccd1dc72e37318653b127a1be0c6d4
Sasha Soch <sochavaag@gmail.com>
2018-10-17 14:02:50 +0300

    PEN timer-count

--- pens/buttons/js/main.js
+++ pens/buttons/js/main.js
@@ -9,30 +9,55 @@
 
 
     /* button 9 */
+    var agDownloadBtn = $('.js-progress-bar_btn'),
+        agDownloadBtnStatus = $('.js-progress-bar_btn__status'),
+        agDownloadBtnTrack = $('.js-progress-bar_btn__track'),
+        agDownloadBtnComplete = $('.js-progress-bar_btn__complete');
+
     function agProgressInterval() {
-      var i = 1,
-      agStatus = $('.js-progress-bar_btn__status'),
-      agTrack = $('.js-progress-bar_btn__track'),
-      agComplete = $('.js-progress-bar_btn__complete');
+      var i = 1;
 
       var agTimerId = setInterval(function() {
-        var progress = (100 - i);
+        var agProgress = (100 - i);
 
         if(i < 100) {
-          agStatus.text(i + '%');
+          agDownloadBtnStatus.text(i + '%');
         }else{
-          agStatus.text('Button 9');
+          agDownloadBtnStatus.text('Button 9');
+          agDownloadBtnStatus.removeClass('js-complete');
         }
-        agTrack.css({'left': '-' + progress + '%'});
-        agComplete.css({'left': progress + '%'});
+        agDownloadBtnTrack.css({'left': '-' + agProgress + '%'});
+        agDownloadBtnComplete.css({'left': agProgress + '%'});
+        agDownloadBtnStatus.addClass('js-complete');
 
         if (i == 100) clearInterval(agTimerId);
         ++i;
       }, 200);
     }
 
-    agProgressInterval();
+      agDownloadBtn.click(function (e) {
+        if(!agDownloadBtnStatus.hasClass('js-complete')) {
+          agProgressInterval();
+        }
+      });
     /* /button 9 */
+
+    /* button 10 */
+    function agAnimateBtnBorderShow() {
+      var agSpeed = 300,
+        agBtnBorder = $('.js-base_btn-v10').children('.js-ag-base_btn-border-v10');
+
+      agBtnBorder.filter('.js-ag-base_btn-border__top-v10').removeAttr('style').animate({'width': 100 + '%'}, agSpeed, function () {
+        agBtnBorder.filter('.js-ag-base_btn-border__right-v10').removeAttr('style').animate({'height': 100 + '%'}, agSpeed, function () {
+          agBtnBorder.filter('.js-ag-base_btn-border__bottom-v10').removeAttr('style').animate({'width': 100 + '%'}, agSpeed, function () {
+            agBtnBorder.filter('.js-ag-base_btn-border__left-v10').removeAttr('style').animate({'height': 100 + '%'}, agSpeed);
+          });
+        });
+      });
+    }
+
+    agAnimateBtnBorderShow();
+
 
   });
 })(jQuery);
---
+++ pens/timer-count/js/main.js
@@ -0,0 +1,36 @@
+var austDay = new Date(2019, 1 - 1, 1, 1, 00);
+$('#ag-countdown').countdown({
+    until: austDay,
+    format: 'dHMs',
+    padZeroes: true,
+    layout: '<ul class="ag-countdown_date">' +
+                    '<li class="ag-countdown_date-box">' +
+                      '<ul class="ag-countdown_days">' +
+                        '<li class="ag-countdown_digits ag-countdown_digit-{d10}"></li>' +
+                        '<li class="ag-countdown_digits ag-countdown_digit-{d1}"></li>' +
+                      '</ul>' +
+                      '<div class="ag-countdown_label">days</div>' +
+                    '</li>' +
+                    '<li class="ag-countdown_date-box">' +
+                      '<ul class="ag-countdown_hours">' +
+                        '<li class="ag-countdown_digits ag-countdown_digit-{h10}"></li>' +
+                        '<li class="ag-countdown_digits ag-countdown_digit-{h1}"></li>' +
+                      '</ul>' +
+                      '<div class="ag-countdown_label">hours</div>' +
+                    '</li>' +
+                    '<li class="ag-countdown_date-box">' +
+                      '<ul class="ag-countdown_minutes">' +
+                        '<li class="ag-countdown_digits ag-countdown_digit-{m10}"></li>' +
+                        '<li class="ag-countdown_digits ag-countdown_digit-{m1}"></li>' +
+                      '</ul>' +
+                      '<div class="ag-countdown_label">minutes</div>' +
+                    '</li>' +
+                    '<li class="ag-countdown_date-box">' +
+                      '<ul class="ag-countdown_seconds">' +
+                        '<li class="ag-countdown_digits ag-countdown_digit-{s10}"></li>' +
+                        '<li class="ag-countdown_digits ag-countdown_digit-{s1}"></li>' +
+                      '</ul>' +
+                      '<div class="ag-countdown_label">seconds</div>' +
+                  '</li>' +
+                '</ul>'
+  });
