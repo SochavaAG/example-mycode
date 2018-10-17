debd1eb96876ec7cd7f878e02547c1e99a4194dc
Sasha Soch <sochavaag@gmail.com>
2018-10-17 16:47:08 +0300

    PEN timer-countdown

--- pens/timer-countdown/js/main.js
+++ pens/timer-countdown/js/main.js
@@ -11,15 +11,15 @@
         start: true,
         refresh_interval: 0.1,
         count_past_zero: true,
-        circle_bg_color: '#60686F',
+        circle_bg_color: '#DEDEDE',
         use_background: true,
-        fg_width: 0.02,
-        bg_width: 0.05,
+        fg_width: 0.05,
+        bg_width: 0.50,
         time: {
-          Days: {show: true, text: 'Days', color: '#FC6'},
-          Hours: {show: true, text: 'Hours', color: '#9CF'},
-          Minutes: {show: true, text: 'Minutes', color: '#BFB'},
-          Seconds: {show: true, text: 'Seconds', color: '#F99'}
+          Days: {show: true, text: 'Days', color: '#F09217'},
+          Hours: {show: true, text: 'Hours', color: '#F09217'},
+          Minutes: {show: true, text: 'Minutes', color: '#F09217'},
+          Seconds: {show: true, text: 'Seconds', color: '#F09217'}
         }
       }).rebuild();
     }
