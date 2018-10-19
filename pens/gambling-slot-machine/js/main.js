f92818711384b7d6d96a0190634bb08a3fd5d709
Sasha Soch <sochavaag@gmail.com>
2018-10-19 11:15:42 +0300

    PEN gambling-slot-machine

--- pens/gambling-slot-machine/js/main.js
+++ pens/gambling-slot-machine/js/main.js
@@ -1,26 +1,31 @@
+(function ($) {
+  $(function () {
 
-function logo() {
-  if (0 < $('#js-gambling-slot').length) {
-    var k = $('.js-gambling-slot_item').length;
-    setInterval(function () {
-      $('.js-gambling-slot_item').addClass('js-ag-animate');
-      $('.js-logo_light').css('display', 'none');
-      $('.js-gambling-slot_letter').animate({top: '39px', display: 'none'}, 400, function () {
-        $(this).css('top', '-39px');
-      });
-      setTimeout(function () {
-        var m = 0,
-          r = setInterval(function () {
-          $('#js-gambling-slot-' + m + ' .js-gambling-slot_letter').animate({top: '0px', display: 'block'}, 150);
-          $('#js-gambling-slot-' + m).removeClass('js-ag-animate');
-          m == k && (setTimeout(function () {
-            $('.js-logo_light').css('display', 'block');
-          }, 175), clearInterval(r));
-          m++
-        }, 150)
-      }, 1700)
-    }, 5000)
-  }
-}
+    function agGamblingSlotMachine() {
+      if (0 < $('#js-gambling-slot').length) {
+        var k = $('.js-gambling-slot_item').length;
+        setInterval(function () {
+          $('.js-gambling-slot_item').addClass('js-ag-animate');
+          $('.js-logo_light').css('display', 'none');
+          $('.js-gambling-slot_letter').animate({top: '39px', display: 'none'}, 400, function () {
+            $(this).css('top', '-39px');
+          });
+          setTimeout(function () {
+            var m = 0,
+              r = setInterval(function () {
+                $('#js-gambling-slot-' + m + ' .js-gambling-slot_letter').animate({top: '0px', display: 'block'}, 150);
+                $('#js-gambling-slot-' + m).removeClass('js-ag-animate');
+                m == k && (setTimeout(function () {
+                  $('.js-logo_light').css('display', 'block');
+                }, 175), clearInterval(r));
+                m++
+              }, 100)
+          }, 1700)
+        }, 5000)
+      }
+    }
 
-logo();
+    agGamblingSlotMachine();
+
+  });
+})(jQuery);
