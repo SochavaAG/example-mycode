b889f711db3d46bd6773da7ac6e9af6f53509fd1
Sasha Soch <sochavaag@gmail.com>
2018-10-23 16:24:54 +0300

    PEN menu-glide-hover

--- pens/menu-glide-hover/js/main.js
+++ pens/menu-glide-hover/js/main.js
@@ -3,7 +3,8 @@ var agHeaderMenu = $('#ag-header-menu');
 agHeaderMenu.lavalamp({
   easing: 'easeOutBack',
   setOnClick: true,
-  speed: 700
+  speed: 700,
+  activeObj:  '.active' // Selector for the active element
 });
 
 agHeaderMenu.children('.js-menu-cat_item').on('click', function () {
