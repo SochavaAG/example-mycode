43b7d2ab7a74ea507ed9fdb57934fce3a2fc2427
Sasha Soch <sochavaag@gmail.com>
2018-11-02 15:40:27 +0200

    PEN eyes-of-man

--- pens/eyes-of-man/js/main.js
+++ pens/eyes-of-man/js/main.js
@@ -6,30 +6,43 @@
       $document = $(document),
       agEyes = agMan.find('.js-man_eyes');
 
-      setTimeout(function() {
-        agMan.addClass('js-ag-man_show');
-      }, 2000);
 
-      agMan.on('click', function(event) {
+    agMan.on('click', function (event) {
+      event.stopPropagation();
+      agMan.toggleClass('js-ag-man_open');
+
+      $document.on('click', function agCloseMan(event) {
         event.stopPropagation();
-        agMan.toggleClass('js-ag-man_open');
 
-        $document.on('click', function agCloseMan(event) {
-          event.stopPropagation();
+        var $target = $(event.target);
 
-          var $target = $(event.target);
-
-          if (!$target.hasClass('js-man') || !$target.parents('.js-man').length) {
-            $document.off('click', agCloseMan);
-            agMan.removeClass('js-ag-man_open');
-          }
-        });
+        if (!$target.hasClass('js-man') || !$target.parents('.js-man').length) {
+          $document.off('click', agCloseMan);
+          agMan.removeClass('js-ag-man_open');
+        }
       });
+    });
 
-      $window.on('mousemove', function(event) {
-        agEyes.css('background-position', (25 * (event.clientX / $window.width()) + 13) + 'px 0');
-        agEyes.css('top', (10 * (event.clientY / $window.height()) + 115) );
-      });
+    $window.on('mousemove', function (event) {
+      agEyes.css('background-position', (25 * (event.clientX / $window.width()) + 13) + 'px 0');
+      agEyes.css('top', (10 * (event.clientY / $window.height()) + 115));
+    });
+
+
+    var agScrollTop;
+
+    $window.on('scroll', (function agOnScroll() {
+      agScrollTop = $window.scrollTop() + $window.height();
+
+      if (agScrollTop > ($('.js-man-page').height() - 50)) {
+        agMan.addClass('js-ag-man_show');
+      } else {
+        agMan.removeClass('js-ag-man_show');
+      }
+
+      return agOnScroll;
+    })());
+
 
   });
 })(jQuery);
