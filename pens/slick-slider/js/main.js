9e311cf75f402642c92460ed54094e1872ba781e
Sasha Soch <sochavaag@gmail.com>
2019-04-26 10:11:07 +0300

    backup

---
+++ pens/slick-slider/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+  $(function () {
+
+
+    $('#js-h-reviews-slides').slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      focusOnSelect: true,
+      infinite: true,
+      pauseOnDotsHover: true,
+      dots: true,
+      dotsClass: 'js-ag-h-reviews-slides_dots-box',
+      customPaging : function(slider, i) {
+        return '<i class="ag-h-reviews-slides_dot"></i>';
+      },
+      nextArrow: '.js-h-reviews-slides_arrow__prev',
+      prevArrow: '.js-h-reviews-slides_arrow__next'
+    });
+
+
+  });
+})(jQuery);
