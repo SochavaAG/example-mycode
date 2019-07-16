9d7b3dcfffddd3cf65b89636c6738e0171b40720
Sasha Soch <sochavaag@gmail.com>
2019-07-16 11:48:29 +0300

    PEN animation-popup

--- pens/animation-popup/js/main.js
+++ pens/animation-popup/js/main.js
@@ -1,7 +1,9 @@
 (function ($) {
   $(function () {
 
-
+    $('.js-modal-popup_btn__close').on('click', function () {
+      $('.js-popup-animation').toggleClass('js-ag-popup-animation-out js-ag-popup-animation-in');
+    });
 
   });
 })(jQuery);
