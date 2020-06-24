da07aa3acfaf1ad04976276095bf3cc3878e3c2a
Sasha Soch <sochavaag@gmail.com>
2020-06-24 14:13:28 +0300

    Scrolling change images

--- pens/testScrolling/index.js
+++ pens/testScrolling/index.js
@@ -1,34 +1,36 @@
-var html = document.querySelector('body');
-var canvas = document.getElementById("hero-lightpass");
+var html = document.querySelector("html");
+var canvas = document.getElementById("js-scrolling-change-images__canvas");
 var context = canvas.getContext("2d");
 
 var frameCount = 255;
 
 var currentFrame = function (index) {
-    return 'images/dd' + index.toString() + '.jpg';
+  return "images/dd" + index.toString() + ".jpg";
 }
 
 var preloadImages = function () {
-    for (let i = 1; i < frameCount; i++) {
-      var img = new Image();
-      img.src = currentFrame(i);
-    }
+  for (var i = 1; i < frameCount; i++) {
+    var img = new Image();
+    img.src = currentFrame(i);
+  }
 };
 
-var img = new Image()
+var img = new Image();
+
 img.src = currentFrame(1);
 canvas.width=2500;
 canvas.height=1440;
-img.onload=function(){
+
+img.onload = function(){
   context.drawImage(img, 0, 0);
 }
 
-var updateImage = function (index) {    
-    img.src = currentFrame(index);  
-    context.drawImage(img, 0, 0);
-  }
+var updateImage = function (index) {
+  img.src = currentFrame(index);
+  context.drawImage(img, 0, 0);
+}
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", function() {
   var scrollTop = html.scrollTop;
   var maxScrollTop = html.scrollHeight - window.innerHeight;
   var scrollFraction = scrollTop / maxScrollTop;
@@ -37,10 +39,10 @@ window.addEventListener('scroll', () => {
     Math.ceil(scrollFraction * frameCount)
   );
 
-  requestAnimationFrame(() => {    
-      if(frameCount !== frameIndex + 1) {
-        updateImage(frameIndex + 1)
-      }
+  requestAnimationFrame(function() {
+    if(frameCount !== frameIndex + 1) {
+      updateImage(frameIndex + 1);
+    }
   })
 });
 
