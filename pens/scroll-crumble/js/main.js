512cc3b8c6edec2ed8eab2cecb9a1c77114463a9
Sasha Soch <sochavaag@gmail.com>
2020-08-14 12:51:23 +0300

    PEN scroll-crumble

--- pens/scroll-crumble/js/main.js
+++ pens/scroll-crumble/js/main.js
@@ -1,128 +1,26 @@
 (function ($) {
   $(function () {
 
-    var controller = new ScrollMagic.Controller();
+    var controller = new ScrollMagic.Controller(),
+    scene;
 
+    for (var i = 1; i < 13; i++) {
+      agFrame = "#frame" + i;
+      agTriggerActive = "#trigger" + i;
+      agTriggerHide = "#trigger" + (i + 1);
 
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger1"})
-      .setClassToggle("#frame1", "active")
-      .addTo(controller)
-      .triggerHook(0.3);
+      agTriggerFrameActive(agTriggerActive, agFrame);
 
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger2"})
-      .setClassToggle("#frame2", "active")
-      .addTo(controller)
-      .triggerHook(0.3);
+      agTriggerFrameHide(agTriggerHide, agFrame);
+    }
 
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger3"})
-      .setClassToggle("#frame3", "active")
-      .addTo(controller)
-      .triggerHook(0.3);
+    function agTriggerFrameActive(agTrigger, agFrame) {
+      scene = new ScrollMagic.Scene({triggerElement: agTrigger}).setClassToggle(agFrame, "active").addTo(controller).triggerHook(0.3);
+    }
 
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger4"})
-      .setClassToggle("#frame4", "active")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger5"})
-      .setClassToggle("#frame5", "active")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger6"})
-      .setClassToggle("#frame6", "active")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger7"})
-      .setClassToggle("#frame7", "active")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger8"})
-      .setClassToggle("#frame8", "active")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger9"})
-      .setClassToggle("#frame9", "active")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger10"})
-      .setClassToggle("#frame10", "active")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger11"})
-      .setClassToggle("#frame11", "active")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger12"})
-      .setClassToggle("#frame12", "active")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger2"})
-      .setClassToggle("#frame1", "hide")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger3"})
-      .setClassToggle("#frame2", "hide")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger4"})
-      .setClassToggle("#frame3", "hide")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger5"})
-      .setClassToggle("#frame4", "hide")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger6"})
-      .setClassToggle("#frame5", "hide")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger7"})
-      .setClassToggle("#frame6", "hide")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger8"})
-      .setClassToggle("#frame7", "hide")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger9"})
-      .setClassToggle("#frame8", "hide")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger10"})
-      .setClassToggle("#frame9", "hide")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger11"})
-      .setClassToggle("#frame10", "hide")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger12"})
-      .setClassToggle("#frame11", "hide")
-      .addTo(controller)
-      .triggerHook(0.3);
-
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger13"})
-      .setClassToggle("#frame12", "hide")
-      .addTo(controller)
-      .triggerHook(0.3);
+    function agTriggerFrameHide(agTrigger, agFrame) {
+      scene = new ScrollMagic.Scene({triggerElement: agTrigger}).setClassToggle(agFrame, "hide").addTo(controller).triggerHook(0.3);
+    }
 
   });
 })(jQuery);
