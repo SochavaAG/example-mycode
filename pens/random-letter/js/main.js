b64414f184ddde7893dbad84c57bc537ade72eb7
Sasha Soch <sochavaag@gmail.com>
2021-03-26 12:44:44 +0200

    PEN random-letter

---
+++ pens/random-letter/js/main.js
@@ -0,0 +1,61 @@
+(function ($) {
+  $(function () {
+
+    function d(c, b) {
+      var a = document.createElement("div");
+      s0(a, "position:absolute;left:" + r(800) + "px;top:" + r(400) + "px;");
+      s(a, "font-size:" + c + "px; opacity: 1.0;");
+      s(a, "color:rgb(" + r(255) + "," + r(255) + "," + r(255) + ");");
+      document.body.appendChild(a);
+      a.innerHTML = agInteger1 = agRandomInteger(41, 59);
+      var b = setInterval(function () {
+        m(a)
+      }, b)
+    }
+    function s0(b, a) {
+      b.style.cssText = a
+    }
+    function s(b, a) {
+      b.style.cssText += a
+    }
+    function m(a) {
+      o = parseFloat(a.style.opacity) - 0.01;
+      a.style.opacity = o;
+      if (o < 0.1) {
+        s0(a, "position:absolute;left:" + r(800) + "px;top:" + r(400) + "px;");
+        s(a, "color:rgb(" + r(255) + "," + r(255) + "," + r(255) + ");");
+        s(a, "font-size:" + (14 + r(72)) + "px; opacity:1.0;");
+
+        a.innerHTML = agInteger1 = agRandomInteger(61, 79);
+      }
+    }
+
+    function r(a) {
+      return Math.round(a * Math.random())
+    }
+    var canvas = document.getElementById("c");
+    var context = canvas.getContext("2d");
+    document.body.style.cssText = "background-color:black;color:white;";
+
+    function l(a) {
+      if (a > 0) {
+        try {
+          d(14 + r(72), 10)
+        } catch (b) {}
+        setTimeout(function () {
+          l(a - 1)
+        }, 100)
+      }
+    }
+    l(50);
+
+    var agRandomInteger = function(argMin, argMax) {
+      var agRand = argMin + Math.random() * (argMax + 1 - argMin);
+
+      var unicode = eval('"\\u00' + Math.floor(agRand) + '"');
+
+      return unicode;
+    };
+
+  });
+})(jQuery);
