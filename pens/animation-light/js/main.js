e21e1d7f359c9d4e2cd96f0a19397f6b7d19103f
Sasha Soch <sochavaag@gmail.com>
2021-04-05 12:17:16 +0300

    PEN animation-light

--- pens/animation-light/js/main.js
+++ pens/animation-light/js/main.js
@@ -1,7 +1,33 @@
 (function ($) {
   $(function () {
 
+    var agActive = 0, agCount, agSpeed = 3000,
+      agItem = $('.js-getbonus_item'),
+      agCount = $('.js-getbonus_item').length;
 
+    agSlideStart();
+
+    function agSlideStart() {
+      agTimerId = setInterval(function () {
+        agSlide();
+      }, agSpeed);
+    }
+
+    function agSlideRemoveClass() {
+      agItem.removeClass('js-ag-active');
+    }
+
+    function agSlide() {
+      agSlideRemoveClass();
+
+      if (agActive == agCount - 1) {
+        agActive = 0;
+      } else {
+        agActive = agActive + 1;
+      }
+
+      $('.js-getbonus_item:eq(' + agActive + ')').addClass('js-ag-active');
+    }
 
   });
 })(jQuery);
