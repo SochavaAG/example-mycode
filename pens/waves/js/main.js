b65bb7023d1ef76c4a82369e3c552ae76546b011
Sasha Soch <sochavaag@gmail.com>
2021-04-05 15:13:42 +0300

    PEN waves

--- pens/waves/js/main.js
+++ pens/waves/js/main.js
@@ -1,7 +1,22 @@
 (function ($) {
   $(function () {
 
+    var agSpeed = 3000,
+      agItem = $('.js-island_survivor');
 
+
+    agActiveStart();
+
+    function agActiveStart() {
+      agTimerId = setInterval(function () {
+        agActive();
+      }, agSpeed);
+    }
+
+
+    function agActive() {
+      agItem.toggleClass('js-ag-active');
+    }
 
   });
 })(jQuery);
