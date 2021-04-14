bca820c8ea6a7fe56bf3a12a9d25810579002b14
Sasha Soch <sochavaag@gmail.com>
2021-04-14 17:54:09 +0300

    PEN animation-ray-and-arrow-item

--- pens/animation-ray-and-arrow-item/js/main.js
+++ pens/animation-ray-and-arrow-item/js/main.js
@@ -2,6 +2,29 @@
   $(function () {
 
 
+    var agDisplayWidth = $(window).width();
+
+    if (agDisplayWidth > 767) {}
+
+    setInterval(fnRay, 1000);
+
+    function fnRay() {
+      $('.js-ray').each(function () {
+        var sty = window.getComputedStyle(this, null);
+        var xform = sty.getPropertyValue('transform');
+        var values = xform.split('(')[1],
+          values = values.split(')')[0],
+          values = values.split(',');
+        var a = values[0];
+        var b = values[1];
+        var angle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
+        var newAng = angle + Math.round(Math.random() * 10 - 5);
+        var rayH = Math.round(Math.random() * 30);
+        var tl = new TimelineMax();
+        tl.to(this, 1, {height: rayH, rotation: newAng, ease: Power0.easeNone})
+      });
+    }
+
 
   });
 })(jQuery);
