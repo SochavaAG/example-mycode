65b7bd4f3d4081869839ee8722aef8bb42d4a08e
Sasha Soch <sochavaag@gmail.com>
2021-05-28 19:04:35 +0300

    PEN wheel-rotate

--- pens/wheel-rotate/js/main.js
+++ pens/wheel-rotate/js/main.js
@@ -1,7 +1,24 @@
 (function ($) {
   $(function () {
 
+var agWheelBtn = $('.js-wheel_btn');
 
+    agWheelBtn.on('click', function () {
+      fnSpinWheel();
+    });
+
+     function fnSpinWheel () {
+      var agWheel = $('.js-wheel'),
+        agWheelSpinNum = $('.js-wheel-spin_num');
+
+      return agWheel.hasClass("js-stage-1") && agWheel.hasClass("js-stage-1-complete") ? (agWheel.addClass("js-spin-to-start_three"), agWheel.removeClass("js-stage-1"), void setTimeout((function () {
+          agWheel.removeClass("js-spin-to-start_three"), agWheel.addClass("js-stage-2"), setTimeout((function () {
+            agWheelSpinNum.text('×');
+          }), 1000)
+        }), 3600)) : agWheel.hasClass("js-stage-1") ? (agWheel.addClass("js-spin-to-start_two"), agWheelSpinNum.text('1'), agWheel.removeClass("js-stage-1"), void setTimeout((function () {
+            agWheel.removeClass("js-spin-to-start_two"), agWheel.addClass("js-stage-1"), agWheel.addClass("js-stage-1-complete"), agWheelSpinNum.text('2');
+          }), 3600)) : void 0;
+    };
 
   });
 })(jQuery);
