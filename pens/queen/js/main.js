ca701588a0972f91507693d71058145193c1e295
Sasha Soch <sochavaag@gmail.com>
2021-06-09 16:16:27 +0300

    PEN animation-sprite

--- pens/queen/js/main.js
+++ pens/queen/js/main.js
@@ -2,6 +2,21 @@
   $(function () {
 
 
+    $('.js-queen-2').hover(function() {
+      $(this).addClass('js-ag-queen');
+    }, function() {
+      $(this).removeClass('js-ag-queen');
+    });
+
+
+    function fnToggle () {
+      $('.js-queen-3').toggleClass('js-ag-queen');
+    }
+
+    setInterval(function () {
+      fnToggle ();
+    }, 1000);
+
 
   });
 })(jQuery);
