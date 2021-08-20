eebb81fd9920c25a723706776282dd7f17ca745d
Sasha Soch <sochavaag@gmail.com>
2021-08-20 15:30:08 +0300

    PEN scroll-wheel-control

--- pens/scroll-wheel-control/js/main.js
+++ pens/scroll-wheel-control/js/main.js
@@ -9,17 +9,18 @@
       responsiveWidth: 0,
       responsiveHeight: 0,
       responsiveSlides: false,
+      anchors: ['js-wheel_page-anchor-1', 'js-wheel_page-anchor-2', 'js-wheel_page-anchor-3', 'js-wheel_page-anchor-4', 'js-wheel_page-anchor-5', 'js-wheel_page-anchor-6', 'js-wheel_page-anchor-7', 'js-wheel_page-anchor-8'],
 
       onLeave: function (e, t) {
-        var r = $('#js-wheel');
+        var agWheel = $('#js-wheel');
 
         $('.js-wheel_page-name').removeClass('js-ag-active');
         $('#js-wheel_page-name-' + (t.index + 1)).addClass('js-ag-active');
 
         if (0 === t.index) {
-          r.css('transform', 'rotate(0deg)');
+          agWheel.css('transform', 'rotate(0deg)');
         }else{
-          r.css('transform', 'rotate(-' + 22.5 * (t.index) + 'deg)');
+          agWheel.css('transform', 'rotate(-' + 22.5 * (t.index) + 'deg)');
         }
       }
     });
