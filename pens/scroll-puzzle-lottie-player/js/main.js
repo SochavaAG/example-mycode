fd67284063c926a26308ca20ddd97bab6af8a936
Sasha Soch <sochavaag@gmail.com>
2021-09-08 10:51:06 +0300

    PEN 1_common

--- pens/scroll-puzzle-lottie-player/js/main.js
+++ pens/scroll-puzzle-lottie-player/js/main.js
@@ -2,6 +2,26 @@
   $(function () {
 
 
+    var player = $('lottie-player')[0];
+    player.load("https://raw.githack.com/SochavaAG/example-mycode/master/pens/scroll-puzzle-lottie-player/js/pzl.json");
+
+    var sWindow = $(window).scrollTop();
+
+    startAnimation(sWindow);
+
+    function startAnimation(argTop) {
+      var agSquare = $('#js-square');
+
+      $(window).bind('scroll load', function () {
+        var argTop = argTop || $(window).scrollTop(),
+          offsetTop = 70,
+          timeVideo = 38,
+          newPercentStart = parseInt((argTop / (agSquare.offset().top - offsetTop)) * timeVideo);
+
+        player.seek(newPercentStart);
+      });
+    }
+
 
   });
 })(jQuery);
