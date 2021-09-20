af34c085f82e1d4effbc941005ff32a8a91ca768
AG Sochava <wikywarenet@gmail.com>
2021-09-20 23:32:46 +0300

    PEN animation-leaf

--- pens/animation-leaf/js/main.js
+++ pens/animation-leaf/js/main.js
@@ -2,6 +2,43 @@
   $(function () {
 
 
+    var e = document.getElementById('js-ag-leaf_box'),
+      t = window.innerWidth,
+      a = 2 * window.innerHeight;
+
+    for (i = 0; i < 12; i++) {
+      var n = document.createElement('span');
+      n.classList.add('js-ag-leaf');
+
+      TweenLite.set(n, {x: fnRandom(0, 2), y: fnRandom(8, a), z: fnRandom(-200, 200)}), e.appendChild(n), r(n);
+    }
+
+    function r(e) {
+      TweenMax.to(e, fnRandom(8, 15), {
+        x: t + 100,
+        ease: Linear.easeNone,
+        repeat: -1,
+        delay: -15
+      }), TweenMax.to(e, fnRandom(4, 2), {
+        y: "+=100",
+        rotationZ: fnRandom(0, 180),
+        repeat: -1,
+        yoyo: true,
+        ease: Sine.easeInOut
+      }), TweenMax.to(e, fnRandom(2, 2), {
+        rotationX: fnRandom(0, 360),
+        rotationY: fnRandom(0, 360),
+        repeat: -1,
+        yoyo: true,
+        ease: Sine.easeInOut,
+        delay: -5
+      })
+    }
+
+    function fnRandom(e, t) {
+      return e + Math.random() * (t - e);
+    }
+
 
   });
 })(jQuery);
