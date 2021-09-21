819c1868ac53f12fb0f0c332ddfb3e762712b03e
Sasha Soch <sochavaag@gmail.com>
2021-09-21 18:58:52 +0300

    PEN swiper-slider-control-progress

--- pens/swiper-slider-control-progress/js/main.js
+++ pens/swiper-slider-control-progress/js/main.js
@@ -2,6 +2,35 @@
   $(function () {
 
 
+    setTimeout(function () {
+      var agMainSlider = new Swiper('.swiper-container', {
+        speed: 500,
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loopedSlides: 1,
+        autoplay: {
+          delay: 4500,
+          autoplayDisableOnInteraction: false
+        },
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+        noSwiping: true,
+        noSwipingClass: 'swiper-container',
+        on: {
+          slideChange: function (swiper) {
+            if (typeof(swiper) != 'undefined') {
+              $('.swiper-slide').width($('.main_slider').width());
+
+              $('.swiper-slide').eq(swiper.activeIndex + 1).width(document.querySelector('.js-main_slider').getBoundingClientRect().width + 1);
+            }
+          }
+        }
+      });
+    }, 500);
+
 
   });
 })(jQuery);
