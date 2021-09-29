70e7fd308e02a911bc3f284cfbf3f0f84169d15a
Sasha Soch <sochavaag@gmail.com>
2021-09-29 10:32:37 +0300

    PEN grid-grid

--- pens/0-120-range/js/main.js
+++ pens/0-120-range/js/main.js
@@ -13,17 +13,17 @@
       agSlider.each(function () {
 
         agValue.each(function () {
-          var value = $(this).prev().val();
+          var agValuePoint = $(this).prev().val();
 
-          $(this).text(value + "%");
-          agFill.width(((value * 100) / 120) + '%');
+          $(this).text(agValuePoint + "%");
+          agFill.width(((agValuePoint * 100) / 120) + '%');
 
-          if (value <= 50) {
+          if (agValuePoint <= 50) {
             agNum.text('1');
             agNum.removeClass('js-ag-range_sum-100').removeClass('js-ag-range_sum-500');
             agValue.removeClass('js-ag-range-slider_value-100').removeClass('js-ag-range-slider_value-500');
             agFill.removeClass('js-ag-range-slider_fill-100').removeClass('js-ag-range-slider_fill-500');
-          } else if (value < 120) {
+          } else if (agValuePoint < 120) {
             agNum.text('100');
             agNum.removeClass('js-ag-range_sum-500').addClass('js-ag-range_sum-100');
             agValue.removeClass('js-ag-range-slider_value-500').addClass('js-ag-range-slider_value-100');
@@ -35,12 +35,12 @@
             agFill.removeClass('js-ag-range-slider_fill-100').addClass('js-ag-range-slider_fill-500');
           }
 
-          if (value < 90) {
-            agValue.css({'left': ((value * 100) / 120) + '%', 'right': 'auto'});
+          if (agValuePoint < 90) {
+            agValue.css({'left': ((agValuePoint * 100) / 120) + '%', 'right': 'auto'});
           } else {
             agValue.css({'left': 'auto', 'right': '0'});
           }
-          if (value > 50) {
+          if (agValuePoint > 50) {
             if (agDisplayWidth < '768') {
               agValue.css({'top': '20px'});
             } else {
@@ -49,7 +49,7 @@
           } else {
             agValue.css({'top': '20px'});
           }
-          agValue.text(Math.round(value) + '%');
+          agValue.text(Math.round(agValuePoint) + '%');
         });
 
         agRange.on('input', function () {
