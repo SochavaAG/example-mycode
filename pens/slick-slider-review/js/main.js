3606d2eaacbb38a56fa4e8a8d5858e7bcba10e8e
SochavaAG <wikywarenet@gmail.com>
2022-01-20 18:36:20 +0200

    slick-slider-review

--- pens/slick-slider-review/js/main.js
+++ pens/slick-slider-review/js/main.js
@@ -2,23 +2,19 @@
   $(function () {
 
 
-    $('body').hasClass('ag-locale-site__RTL') ? reviewsRTL = true : reviewsRTL = false;
-
-
-    $('#js-review-user-info').slick({
+    $('#js-h-reviews-slides').slick({
       autoplay: true,
       autoplaySpeed: 3000,
       focusOnSelect: true,
       infinite: true,
       pauseOnDotsHover: true,
       dots: true,
-      rtl: reviewsRTL,
       dotsClass: 'js-ag-h-reviews-slides_dots-box ag-h-reviews-slides_dots-box',
       customPaging : function(slider, i) {
         return '<i class="ag-h-reviews-slides_dot"></i>';
       },
-      nextArrow: '.js-review-slider_arrow__prev',
-      prevArrow: '.js-review-slider_arrow__next'
+      nextArrow: '.js-h-reviews-slides_arrow__prev',
+      prevArrow: '.js-h-reviews-slides_arrow__next'
     });
 
 
