5bbd31938a44487aab561d6b447666bd9d83b9e2
Aleksandr <wikywarenet@gmail.com>
2023-01-23 03:35:47 +0200

    slider-range-ui

--- pens/slider-range-ui/js/main.js
+++ pens/slider-range-ui/js/main.js
@@ -1,7 +1,46 @@
 (function ($) {
   $(function () {
 
+    $('.js-slider-range').each(function() {
+      var minValue = Number($(this).attr('data-min')),
+        maxValue = Number($(this).attr('data-max')),
+        value = Number($(this).attr('data-value')),
+        step = Number($(this).attr('data-step')),
+        $this = $(this);
 
+      $this.slider({
+        range: true,
+        value: value,
+        min: minValue,
+        max: maxValue,
+        step: step,
+        values: [minValue, maxValue],
+        slide: function(event, ui) {
+          var selectedMin = ui.values[0],
+            selectedMax = ui.values[1],
+            currentValueMin = selectedMin,
+            currentValueMax = selectedMax;
+
+          if(currentValueMax > 59) {
+            currentValueMax = ">" + currentValueMax;
+          }
+
+          $this.find('.js-min-value').html(currentValueMin).attr('data-selected-value', selectedMin);
+          $this.find('.js-max-value').html(currentValueMax).attr('data-selected-value', selectedMax);
+        }
+
+      });
+
+      var currentValueMin = minValue,
+        currentValueMax = maxValue;
+
+      if(currentValueMax > 59) {
+        currentValueMax = ">" + currentValueMax;
+      }
+
+      $this.find('span[tabindex]:first-of-type').append('<span class="js-ag-range_num js-min-value" data-selected-value></span>').find('.js-ag-range_num').html(currentValueMin).attr('data-selected-value', minValue);
+      $this.find('span[tabindex]:last-of-type').append('<span class="js-ag-range_num js-max-value" data-selected-value></span>').find('.js-ag-range_num').html(currentValueMax).attr('data-selected-value', maxValue);
+    });
 
   });
 })(jQuery);
