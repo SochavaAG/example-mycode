de28a15e353825ea83198cfe334bc1caf2f4101e
Aleksandr <wikywarenet@gmail.com>
2023-10-12 22:34:11 +0300

    slick-slider-vertical

--- pens/slick-slider-vertical/js/main.js
+++ pens/slick-slider-vertical/js/main.js
@@ -2,6 +2,42 @@
   $(function () {
 
 
+    $('.js-first-slider').slick({
+      vertical: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      verticalSwiping: true,
+      centerMode: true,
+      centerPadding: '0px',
+      infinite: false,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            vertical: false,
+            verticalSwiping: false,
+            centerPadding: '0px',
+            slidesToShow: 3,
+            mobileFirst: true,
+            initialSlide: 1,
+            slidesToScroll: 1,
+            infinite: true
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            vertical: false,
+            verticalSwiping: false,
+            centerPadding: '0px',
+            slidesToShow: 1,
+            mobileFirst: true,
+            initialSlide: 1,
+            slidesToScroll: 1
+          }
+        }]
+    });
+
 
   });
 })(jQuery);
