06c055ba1e38c2be0da49ecfefebce0fedcd31ab
Aleksandr <wikywarenet@gmail.com>
2023-10-27 17:37:53 +0300

    PEN carousel-slick-zoom

--- pens/carousel-slick-zoom/js/main.js
+++ pens/carousel-slick-zoom/js/main.js
@@ -1,44 +1,6 @@
 (function ($) {
   $(function () {
 
-    /* doctor certificate carousel */
-   /* $('.js-doctor-certificate-carousel').slick({
-      infinite: true,
-      autoplay: true,
-      autoplaySpeed: 30000,
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      prevArrow: '.js-ag-doctor-certificate-carousel-arrow_prev',
-      nextArrow: '.js-ag-doctor-certificate-carousel-arrow_next',
-
-      variableWidth: true,
-      centerPadding: '50px',
-      /!*centerMode: true,*!/
-
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            centerPadding: '10px',
-            variableWidth: false,
-            centerMode: false
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            centerPadding: '10px',
-            variableWidth: false,
-            centerMode: false
-          }
-        }
-      ]
-    });*/
-    /* /doctor certificate carousel */
-
-
     $('.js-carousel').slick({
       infinite: true,
       autoplay: true,
