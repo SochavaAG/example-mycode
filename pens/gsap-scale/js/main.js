8663a24d5823045b4393c81fc452f45cf1bfe9c9
Aleksandr <wikywarenet@gmail.com>
2023-11-09 04:40:26 +0200

    PEN gsap-scale

--- pens/gsap-scale/js/main.js
+++ pens/gsap-scale/js/main.js
@@ -13,8 +13,6 @@
     var agFlag = 0;
 
     $(window).bind(mousewheelevt, function (event) {
-      //var agFrame = $('#main-scene > div:nth-child(1), #page');
-
       if (agFlag >= 1) {
         TweenMax.to(agDesktop, 1, {scale: 2.5, y: 0});
         TweenMax.to(agFrame, 1, {scale: 1, y: 0, onCompleteParams:[agFrame], onComplete: toggle});
@@ -30,14 +28,6 @@
           agFlag = agFlag + 1;
         }
       }
-
-      /*if (event.originalEvent.wheelDelta >= 0) {
-       TweenMax.to($('#desktop'), 1, {scale: 2.5, y: 0});
-       TweenMax.to($('#main-scene > div:nth-child(1), #page'), 1, {scale: 1, y: 0});
-       }else{
-       TweenMax.to($('#desktop'), 1, {scale: 1, y: -52});
-       TweenMax.to($('#main-scene > div:nth-child(1), #page'), 1, {scale: 0.51, y: -102});
-       }*/
     });
 
   });
